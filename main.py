a0623b27a8a4452272186aa725027db00d7b4042
mrsnurk32 <nickbelik@mail.ru>
2020-12-25 12:54:31 +0300

    asyncio added

--- main.py
+++ main.py
@@ -3,32 +3,34 @@ import os
 
 import threading
 import subprocess
+import asyncio
 
 
-def run_command(video_settings_initial,mining_file,video_settings_final):
-    
-    print('starting new process')
+async def set_video_setting(video_settings_initial, video_settings_final):
 
+    print('setting default video settings')
     os.system(video_settings_initial)
-    print('default video settings')
-    
-    time.sleep(3)
-    
-    print('start mining')
-    process = subprocess.Popen(mining_file, stdout=subprocess.PIPE)
 
-    time.sleep(180)
-    
+    await asyncio.sleep(180)
+
     print('setting new settings')
     os.system(video_settings_final)
-    
 
+
+async def start_mining(mining_file,video_settings_initial,video_settings_final):
+    initial_settings = loop.create_task(set_video_setting(
+        video_settings_initial, video_settings_final)
+    )
+
+    await asyncio.sleep(5)
     
-    counter = 0
+    print('start mining')
+    process = subprocess.Popen(mining_file, stdout=subprocess.PIPE) 
+        
     while True:
-        counter += 1
         time.sleep(1)
         output = process.stdout.readline().decode('cp866')
+
         if output == '' and process.poll() is not None:
             break
         if output:
@@ -37,10 +39,11 @@ def run_command(video_settings_initial,mining_file,video_settings_final):
             if 'Hashrate' in output:
                 line = output.split(' ')
                 hash_rate = line[3]
-                if int(hash_rate) < 200:
+                if float(hash_rate) < 200:
                     process.kill()
                     run_command(video_settings_initial,mining_file,video_settings_final)
-                    
+
+
     rc = process.poll()
     return rc
 
@@ -49,4 +52,16 @@ video_settings_initial = '3070-1.bat'
 mining_file = '!ETH-ethermine.bat'
 video_settings_final = '3070.bat'
 
-run_command(video_settings_initial,mining_file,video_settings_final)
+
+if __name__ == '__main__':
+
+    try:
+        loop = asyncio.get_event_loop()
+        loop.set_debug(1)
+        loop.run_until_complete(start_mining(mining_file,video_settings_initial,))
+    except Exception as e:
+        print(e)
+
+    finally:
+        loop.close()
+
